d45d89a098384531a2166b4fb24136fbf8f39fd6
Lucas Moreira <lucasmsea@outlook.com>
2020-07-10 21:38:41 -0300

    miss anchor cards and info page

--- src/components/Books/index.tsx
+++ src/components/Books/index.tsx
@@ -27,17 +27,15 @@ const BooksComponent: React.FC<BooksProps> = ({
   }
 
   return (
-    <>
-      <BookCard>
-        {thumbnail && <img src={thumbnail} alt="book cover" />}
-        <h2><u>Title</u></h2>
-        <p>{titleDisplay.length > 48 ? titleDisplay.substr(0, 50).concat('...') : titleDisplay}</p>
-        <h2><u>Author</u></h2>
-        <p>{authorDisplay.length > 48 ? authorDisplay.substr(0, 50).concat('...') : authorDisplay}</p>
-        <h2><u>Published Date</u></h2>
-        <p>{publishedDate}</p>
-    </BookCard>
-    </>
+        <BookCard className='card'>
+            {thumbnail && <img src={thumbnail} alt="book cover" />}
+            {titleDisplay && <h2><u>Title</u></h2>}
+            <p>{titleDisplay.length > 48 ? titleDisplay.substr(0, 50).concat('...') : titleDisplay}</p>
+            {authorDisplay && <h2><u>Author(s)</u></h2>}
+            <p>{authorDisplay.length > 48 ? authorDisplay.substr(0, 50).concat('...') : authorDisplay}</p>
+            {publishedDate && <h2><u>Published Date</u></h2>}
+            <p>{publishedDate}</p>
+        </BookCard>
   )
 }
 
--- src/components/Books/styles.ts
+++ src/components/Books/styles.ts
@@ -1,49 +1,59 @@
 import styled from 'styled-components'
 
+
 export const BookCard = styled.div`
-  font-family: 'Roboto', sans-serif;
-  background: #4B148B;
-  margin-top: 100px;
-  margin-left: 650px;
-  min-height: 600px;
 
-  max-width: 290px;
-  min-width: 290px;
-
-  transform: translateY(-470px);
-  padding: 0px 20px 60px 20px;
-  border-radius: 10px;
-  border: 2px solid white;
-  text-align: center;
-  align-content: center;
-  margin-bottom: 10px;
-
-  @media only screen and (max-width: 940px) {
-    transform: translateY(-200px);
-    transform: translateX(60px);
-  }
-  
-  img {
-    margin: 0 20px 30px 20px;
-    transform: translateY(0.01px);
-    border: 1px solid white;
-  }
-
-  h2 {
-    margin-top: 10px;
-    font-family: 'Josefin Slab', serif;
-    font-weight: bold;
-    color: white;
-  }
-
-  p {
-    margin-top: 5px;
-    margin-left: 5px;
-    margin-right: 5px;
-    color: white;
-    margin-bottom: 35px;
-    &:nth-child(7) {
-        margin-bottom: 0;
+    transition: margin-right 0.5s;
+    &:hover {
+      margin-right: -30px;
     }
-  }
+    text-decoration: none;
+    font-family: 'Roboto', sans-serif;
+    background: #4B148B;
+    margin-top: 100px;
+    margin-left: 650px;
+    min-height: 600px;
+
+    max-width: 290px;
+    min-width: 290px;
+
+    transform: translateY(-470px);
+    padding: 0px 20px 60px 20px;
+    border-radius: 10px;
+    border: 2px solid white;
+    text-align: center;
+    align-content: center;
+    margin-bottom: 10px;
+
+    @media only screen and (max-width: 940px) {
+      transform: translateY(-200px);
+      transform: translateX(60px);
+    }
+    
+    img {
+      margin: 0 20px 30px 20px;
+      transform: translateY(0.01px);
+      border: 1px solid white;
+    }
+
+    h2 {
+      margin-top: 10px;
+      font-family: 'Josefin Slab', serif;
+      font-weight: bold;
+      color: white;
+    }
+
+    p {
+      margin-top: 5px;
+      margin-left: 5px;
+      margin-right: 5px;
+      color: white;
+      margin-bottom: 35px;
+      &:nth-child(7) {
+          margin-bottom: 0;
+      }
+    }
+  
+  
+
 `
