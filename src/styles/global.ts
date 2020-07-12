ef1850c96ab87df68a56126871af711875618a41
Lucas Moreira <lucasmsea@outlook.com>
2020-07-11 22:02:06 -0300

    try toremove clipping border from overflow

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -24,6 +24,9 @@ export default createGlobalStyle`
     max-width: 960px;
     margin: 20px auto;
     padding: 40px 10px;
+    @media only screen and (max-width: 493px) {
+      margin-bottom: 60px;
+    }
   }
 
   button {
